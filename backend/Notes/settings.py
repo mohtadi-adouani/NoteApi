c1ef248763046316e588010b996f519b01bfd340
Mohtadi <mohtadi.adouani@gmail.com>
2024-12-29 20:33:47 +0100

    - add cors header

--- backend/Notes/settings.py
+++ backend/Notes/settings.py
@@ -18,7 +18,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 
 # Application definition
@@ -35,6 +37,8 @@ INSTALLED_APPS = [
     # app to manage accounts
     'accounts',
     'Post',
+    # CORS
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -45,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 REST_FRAMEWORK = {
