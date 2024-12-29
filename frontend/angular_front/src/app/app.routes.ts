97b3a721a7c0e49fb6a6a8ef13c82c3d55f2e3bd
Mohtadi <mohtadi.adouani@gmail.com>
2024-12-29 18:27:35 +0100

    - add components footer nav_bar and home - add router

--- frontend/angular_front/src/app/app.routes.ts
+++ frontend/angular_front/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {NotFoundComponent} from '../components/not-found/not-found.component';
+import {HomeComponent} from '../components/home/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '/home', component: HomeComponent},
+  {path: '**', component: NotFoundComponent}
+  ];
