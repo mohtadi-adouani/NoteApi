d8b4484958142dbde33335c866f0a07b1d1eb656
Mohtadi <mohtadi.adouani@gmail.com>
2024-12-29 18:31:48 +0100

    - fix subdirectory error

--- frontend/angular_front/src/app/app.routes.ts
+++ frontend/angular_front/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import {NotFoundComponent} from '../components/not-found/not-found.component';
-import {HomeComponent} from '../components/home/home/home.component';
+import {HomeComponent} from '../components/home/home.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
