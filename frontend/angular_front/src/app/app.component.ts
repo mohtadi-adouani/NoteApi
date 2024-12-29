bdcc8adfcd867d0875588dceae13ac7313c9f32a
Mohtadi <mohtadi.adouani@gmail.com>
2024-12-29 19:36:44 +0100

    - fix nav bar and footer - add login and logout component

--- frontend/angular_front/src/app/app.component.ts
+++ frontend/angular_front/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterOutlet} from '@angular/router';
+import {CommonModule} from '@angular/common';
 import {NavBarComponent} from '../components/nav-bar/nav-bar.component';
 import {FooterComponent} from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/angular_front/src/app/app.routes.ts
+++ frontend/angular_front/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import {NotFoundComponent} from '../components/not-found/not-found.component';
 import {HomeComponent} from '../components/home/home.component';
+import {LoginComponent} from '../components/login/login.component';
+import {LogoutComponent} from '../components/logout/logout.component';
 
 export const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: '/home', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'logout', component: LogoutComponent},
+  {path: '', redirectTo:'home', pathMatch: 'full'},
   {path: '**', component: NotFoundComponent}
   ];
