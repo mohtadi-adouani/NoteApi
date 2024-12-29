84bab18eaeddb0525abc2c644cdf4a8ac42ece2a
Mohtadi <mohtadi.adouani@gmail.com>
2024-12-29 22:27:22 +0100

    - add dynamic loging check

--- frontend/angular_front/src/components/nav-bar/nav-bar.component.ts
+++ frontend/angular_front/src/components/nav-bar/nav-bar.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import {RouterLink, RouterLinkActive} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RouterLink,} from '@angular/router';
+import {AuthService} from '../../services/auth/auth.service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
   imports: [
     RouterLink,
+    NgIf,
   ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit{
+
+  constructor(private authService: AuthService) {}
+  protected isLogged: boolean | undefined;
+
+
+  ngOnInit(){
+    this.authService.isLogged.subscribe(logged => {
+      this.isLogged = logged;
+    });
+  }
 
 }
--- frontend/angular_front/src/services/auth/auth.service.ts
+++ frontend/angular_front/src/services/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {environment} from '../../environments/environment';
-import {tap} from 'rxjs';
+import {ReplaySubject, tap} from 'rxjs';
 import {Credentials, TokenResponse} from '../../interfaces/credentials';
 
 @Injectable({
@@ -12,7 +12,8 @@ export class AuthService {
   private base_url = environment.api_url;
   private access_header = 'access';
   private refresh_header = 'refresh';
-
+  private logged = new ReplaySubject<boolean>(1);
+  isLogged = this.logged.asObservable();
   private token_response: TokenResponse = new TokenResponse()
   constructor(private http: HttpClient) { }
   login(credentials: Credentials){
@@ -21,19 +22,18 @@ export class AuthService {
         this.token_response = response;
         localStorage.setItem(this.getTokenHeader(), this.token_response.access)
         localStorage.setItem(this.getRefreshHeader(), this.token_response.refresh)
+        this.logged.next(true);
       })
     )
   }
 
   logout(){
     localStorage.clear()
+    this.logged.next(false);
   }
-
-  isAuthentified(){ return localStorage.getItem(this.getTokenHeader()) != null}
   getTokenHeader(){return this.access_header}
   getRefreshHeader(){return this.refresh_header}
   getToken(): string | null {return localStorage.getItem(this.getTokenHeader())}
   getRefresh(): string | null{return localStorage.getItem(this.getRefreshHeader())}
-
 
 }
