bd20814c4949af4a4282a1b86310661acbe4e3fb
Mohtadi <mohtadi.adouani@gmail.com>
2024-12-30 16:47:42 +0100

    - add create update and destroy cruds to posts

--- backend/Post/serializers.py
+++ backend/Post/serializers.py
@@ -10,10 +10,3 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ['id', 'title', 'body', 'author', 'created_at', 'updated_at']
         read_only_fields = ['author', 'created_at', 'updated_at']
-
-    def create(self, validated_data):
-        if validated_data.is_valid():
-            validated_data.save()
-            return validated_data
-        else:
-            return serializers.ValidationError(validated_data.errors)
--- backend/Post/views.py
+++ backend/Post/views.py
@@ -1,4 +1,6 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -15,4 +17,20 @@ class PostListView(ListCreateAPIView):
         queryset = self.get_queryset()
         serializer = PostSerializer(queryset, many=True)
         return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=False)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        # The request user is set as author automatically.
+        serializer.save(author=self.request.user)
+
+class PostDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
 
