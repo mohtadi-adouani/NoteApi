192c0d06dc93eb62a99349bcabc4407e40a25264
Mohtadi <mohtadi.adouani@gmail.com>
2025-01-05 23:51:42 +0100

    - replace bootswatch nav bar with material nav bar

--- frontend/angular_front/src/components/nav-bar/nav-bar.component.ts
+++ frontend/angular_front/src/components/nav-bar/nav-bar.component.ts
@@ -1,13 +1,15 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink,} from '@angular/router';
 import {AuthService} from '../../services/auth/auth.service';
 import {NgIf} from '@angular/common';
+import {MatToolbar} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-nav-bar',
   imports: [
     RouterLink,
     NgIf,
+    MatToolbar,
   ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
