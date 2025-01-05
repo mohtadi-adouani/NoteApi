ca2b12cd68d9cd8ea707a47f64ad0d704977c338
Mohtadi <mohtadi.adouani@gmail.com>
2025-01-05 23:25:52 +0100

    - add materials - remove bootswatch

--- frontend/angular_front/src/app/app.config.ts
+++ frontend/angular_front/src/app/app.config.ts
@@ -4,10 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {TokenInterceptor} from '../interceptors/token.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-    provideHttpClient(withInterceptors([TokenInterceptor]))
+    provideHttpClient(withInterceptors([TokenInterceptor])), provideAnimationsAsync()
   ]
 };
